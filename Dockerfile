fd8296b72e418d8b9ededaf9672852cf8feb304b
jlarriba <jlarriba@redhat.com>
2022-08-05 11:57:49 +0200

    Allow the replacement of settings with environment variables to configure parameters

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM quay.rdoproject.org/tripleomastercentos9/openstack-tripleo-ansible-ee:b979b304ce5b7f7c85e20db171281a9f
 
 ADD test.yaml ./project
+ADD settings ./env
 ADD entrypoint.sh /
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 echo "tempuser:x:$(id -u):$(id -g):,,,:${HOME}:/bin/bash" >> /etc/passwd
 echo "tempuser:x:$(id -G | cut -d' ' -f 2)" >> /etc/group
 
+# Expand the variables
+eval "echo \"$(cat /runner/env/settings)\"" > /runner/env/settings
+
 $1 $2
